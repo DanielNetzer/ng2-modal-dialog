a9f1463e45ea35282327a4765465f10232598162
Daniel Netzer <daniel@peak.studio>
2017-09-27 09:29:08 +0300

    added bundling, minify, uglify, rollup.

--- modal.module.ts
+++ modal.module.ts
@@ -26,26 +26,26 @@ export class ModalService {
     create<T>(module: any, component: any, parameters?: Object):
         Observable<ComponentRef<T>> {
         // we return a stream so we can  access the componentref
-        let componentRef$ = new ReplaySubject<ComponentRef<T>>();
+        let componentRef$: ReplaySubject<ComponentRef<T>> = new ReplaySubject<ComponentRef<T>>();
         // compile the component based on its type and
         // create a component factory
         this.compiler.compileModuleAndAllComponentsAsync(module)
             .then(factory => {
                 // look for the componentfactory in the modulefactory
-                let componentFactory = factory.componentFactories
+                let componentFactory: ComponentFactory<any> = factory.componentFactories
                     .filter(item => item.componentType === component)[0];
-                // the injector will be needed for DI in 
+                // the injector will be needed for DI in
                 // the custom component
-                const childInjector = ReflectiveInjector
+                const childInjector: ReflectiveInjector = ReflectiveInjector
                     .resolveAndCreate([], this.injector);
                 // create the actual component
-                let componentRef = this.vcRef
+                let componentRef: ComponentRef<any> = this.vcRef
                     .createComponent(componentFactory, 0, childInjector);
                 // pass the @Input parameters to the instance
                 Object.assign(componentRef.instance, parameters);
                 this.activeInstances++;
                 // add a destroy method to the modal instance
-                componentRef.instance["destroy"] = () => {
+                componentRef.instance.destroy = () => {
                     this.activeInstances--;
                     // this will destroy the component
                     componentRef.destroy();
@@ -60,13 +60,13 @@ export class ModalService {
 
     createFromFactory<T>(componentFactory: ComponentFactory<T>,
         parameters?: Object): Observable<ComponentRef<T>> {
-        let componentRef$ = new ReplaySubject<ComponentRef<T>>();
-        const childInjector = ReflectiveInjector.resolveAndCreate([], this.injector);
+        let componentRef$: ReplaySubject<ComponentRef<T>> = new ReplaySubject<ComponentRef<T>>();
+        const childInjector: ReflectiveInjector = ReflectiveInjector.resolveAndCreate([], this.injector);
         let componentRef: any = this.vcRef.createComponent(componentFactory, 0, childInjector);
         // pass the @Input parameters to the instance
         Object.assign(componentRef.instance, parameters);
         this.activeInstances++;
-        componentRef.instance["destroy"] = () => {
+        componentRef.instance.destroy = () => {
             this.activeInstances--;
             componentRef.destroy();
         };
@@ -79,7 +79,7 @@ export class ModalService {
 // this is the modal-placeholder, it will container the created modals
 @Component({
     selector: "modal-placeholder",
-    templateUrl: './modal.module.html'
+    templateUrl: "./modal.module.html"
 })
 export class ModalPlaceholderComponent implements OnInit {
     @ViewChild("modalplaceholder", { read: ViewContainerRef }) viewContainerRef: ViewContainerRef;
@@ -93,7 +93,7 @@ export class ModalPlaceholderComponent implements OnInit {
     }
 }
 
-// These 2 items will make sure that you can annotate
+// these 2 items will make sure that you can annotate
 // a modalcomponent with @Modal()
 export class ModalContainer {
     destroy: Function;
@@ -102,8 +102,8 @@ export class ModalContainer {
         this.destroy();
     }
 }
-export function Modal() {
-    return function (target: any) {
+export function Modal(): any {
+    return function (target: any): void {
         Object.assign(target.prototype, ModalContainer.prototype);
     };
 }
